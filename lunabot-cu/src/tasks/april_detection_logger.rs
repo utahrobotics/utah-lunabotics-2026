0dd119c6f49d51596681a35d161335de5d12114b
Matthew Ashton Knochel <ffeufhd5j@relay.firefox.com>
2025-06-16 17:30:48 -0600

    lint

--- lunabot-cu/src/tasks/april_detection_logger.rs
+++ lunabot-cu/src/tasks/april_detection_logger.rs
@@ -1,5 +1,12 @@
-use cu29::{clock::RobotClock, config::ComponentConfig, cutask::{CuSinkTask, Freezable}, input_msg, prelude::*, CuResult};
 use cu29::cutask::CuMsg;
+use cu29::{
+    clock::RobotClock,
+    config::ComponentConfig,
+    cutask::{CuSinkTask, Freezable},
+    input_msg,
+    prelude::*,
+    CuResult,
+};
 use cu_apriltag::AprilTagDetections;
 
 // Sink that logs AprilTag detections
@@ -10,7 +17,10 @@ impl Freezable for DetectionLogger {}
 
 impl<'cl> CuSinkTask<'cl> for DetectionLogger {
     // one detections struct per camera
-    type Input = (input_msg!('cl, AprilTagDetections),input_msg!('cl, AprilTagDetections));
+    type Input = (
+        input_msg!('cl, AprilTagDetections),
+        input_msg!('cl, AprilTagDetections),
+    );
 
     fn new(_config: Option<&ComponentConfig>) -> CuResult<Self> {
         Ok(Self)
@@ -22,31 +32,37 @@ impl<'cl> CuSinkTask<'cl> for DetectionLogger {
         if let Some(dets) = input1.payload() {
             for (id, pose, _) in dets.filtered_by_decision_margin(50.0) {
                 //TODO: poses still are garbage from that one godforsaken bug
-                info!("[{}] Detected tag {} with pose: {}",&dets.camera_id, id, pose);
+                info!(
+                    "[{}] Detected tag {} with pose: {}",
+                    &dets.camera_id, id, pose
+                );
             }
         }
         if let Some(dets) = input2.payload() {
             for (id, pose, _) in dets.filtered_by_decision_margin(50.0) {
                 //TODO: poses still are garbage from that one godforsaken bug
-                info!("[{}] Detected tag {} with pose: {}",&dets.camera_id, id, pose);
+                info!(
+                    "[{}] Detected tag {} with pose: {}",
+                    &dets.camera_id, id, pose
+                );
             }
         }
-        
+
         Ok(())
     }
-    
+
     fn start(&mut self, _clock: &RobotClock) -> CuResult<()> {
         Ok(())
     }
-    
+
     fn preprocess(&mut self, _clock: &RobotClock) -> CuResult<()> {
         Ok(())
     }
-    
+
     fn postprocess(&mut self, _clock: &RobotClock) -> CuResult<()> {
         Ok(())
     }
-    
+
     fn stop(&mut self, _clock: &RobotClock) -> CuResult<()> {
         Ok(())
     }
