0f8bed35ff5a6ce9e99ac4636871963ae7b0ea5e
Matthew Ashton Knochel <matthewashtonknochel@gmail.com>
2025-06-24 18:37:10 -0600

    finally

---
+++ unilidar_sdk2/unitree_lidar_sdk/build/CMakeFiles/set_to_udp_mode.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/matthew_a/lunabotics-cu/unilidar_sdk2/unitree_lidar_sdk/bin/set_to_udp_mode"
+  "/home/matthew_a/lunabotics-cu/unilidar_sdk2/unitree_lidar_sdk/bin/set_to_udp_mode.pdb"
+  "CMakeFiles/set_to_udp_mode.dir/examples/set_to_udp_mode.cpp.o"
+  "CMakeFiles/set_to_udp_mode.dir/examples/set_to_udp_mode.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/set_to_udp_mode.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
