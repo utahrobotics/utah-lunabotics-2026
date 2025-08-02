0d7055c184ddd16a7a7f48b6c05f388ac586974c
Matthew Ashton Knochel <matthewashtonknochel@gmail.com>
2025-08-01 20:27:22 -0600

    formatting

--- lunabot-cu/src/main.rs
+++ lunabot-cu/src/main.rs
@@ -1,23 +1,22 @@
 #![feature(let_chains, try_blocks, f16, result_flattening)]
-pub mod tasks;
-pub mod rerun_viz;
-pub mod utils;
 pub mod comms;
+pub mod rerun_viz;
+pub mod tasks;
+pub mod utils;
 
+use crate::tasks::*;
 use cu29::prelude::*;
 use cu29_helpers::basic_copper_setup;
 use launcher::ProcessCommand;
 use simple_motion::{ChainBuilder, NodeSerde, StaticNode};
+use std::path::{Path, PathBuf};
 use std::sync::OnceLock;
 use std::thread::sleep;
 use std::time::Duration;
-use std::path::{Path,PathBuf};
-use crate::tasks::*;
 
 const PREALLOCATED_STORAGE_SIZE: Option<usize> = Some(1024 * 1024 * 100);
 
 pub static ROOT_NODE: OnceLock<StaticNode> = OnceLock::new();
-
 
 #[copper_runtime(config = "copperconfig.ron")]
 struct LunabotApplication {}
@@ -41,7 +40,6 @@ fn main() {
     }
     launcher.add_command("realsense publisher", realsense_cmd);
 
-
     let mut ai_cmd = ProcessCommand::new("cargo")
         .with_args(vec!["run", "--release"])
         .with_working_directory("../external-tasks/lunabot-ai2");
@@ -57,24 +55,31 @@ fn main() {
             std::fs::create_dir_all(parent).expect("Failed to create logs directory");
         }
     }
-    let copper_ctx =
-        basic_copper_setup(&PathBuf::from(&logger_path), PREALLOCATED_STORAGE_SIZE, true, None).expect("Failed to setup logger.");
+    let copper_ctx = basic_copper_setup(
+        &PathBuf::from(&logger_path),
+        PREALLOCATED_STORAGE_SIZE,
+        true,
+        None,
+    )
+    .expect("Failed to setup logger.");
 
     info!("Logger created at {}.", logger_path);
     info!("Creating application... ");
     info!("Initializing rerun viz... ");
-    rerun_viz::init_rerun(rerun_viz::RerunViz::Viz(rerun_viz::Level::All)).expect("Failed to initialize rerun viz.");
+    rerun_viz::init_rerun(rerun_viz::RerunViz::Viz(rerun_viz::Level::All))
+        .expect("Failed to initialize rerun viz.");
 
     let robot_chain = NodeSerde::from_reader(
         std::fs::File::open("../robot-layout/lunabot.ron").expect("Failed to read robot chain"),
-    ).expect("Failed to parse robot chain");
+    )
+    .expect("Failed to parse robot chain");
     let robot_chain = ChainBuilder::from(robot_chain).finish_static();
     let _ = ROOT_NODE.set(robot_chain);
 
     let mut application = LunabotApplicationBuilder::new()
-            .with_context(&copper_ctx)
-            .build()
-            .expect("Failed to create application.");
+        .with_context(&copper_ctx)
+        .build()
+        .expect("Failed to create application.");
     let clock = copper_ctx.clock.clone();
     info!("Running... starting clock: {}.", clock.now());
 
