c40355fd7772f7c8e97c156bd2e54077521b0b43
Matthew Ashton Knochel <matthewashtonknochel@gmail.com>
2025-08-02 21:22:07 -0600

    remove unused code

--- lunabot-cu/src/tasks/april_detection_handler.rs
+++ lunabot-cu/src/tasks/april_detection_handler.rs
@@ -389,43 +389,8 @@ impl Transform3DFromNa for Transform3D<f64> {
 }
 
 use cu_spatial_payloads::{Transform3D, Transform3DCast};
-use nalgebra::{Isometry3, Point3, Translation3, UnitQuaternion, Vector3};
+use nalgebra::{Isometry3, Translation3, UnitQuaternion, Vector3};
 use rerun::Boxes3D;
-
-#[derive(Deserialize, Clone, Copy)]
-pub struct Apriltag {
-    pub tag_position: Point3<f64>,
-    forward_axis: Vector3<f64>,
-    #[serde(default)]
-    roll: f64,
-    pub tag_width: f64,
-}
-
-impl Apriltag {
-    pub fn get_quat(self) -> UnitQuaternion<f64> {
-        // First rotation to face along the forward axis
-        let rotation1 =
-            UnitQuaternion::rotation_between(&Vector3::new(0.0, 0.0, -1.0), &self.forward_axis)
-                .unwrap_or(UnitQuaternion::from_scaled_axis(Vector3::new(
-                    0.0,
-                    std::f64::consts::PI,
-                    0.0,
-                )));
-
-        let cross_axis = self.forward_axis.cross(&Vector3::new(0.0, 1.0, 0.0));
-        let true_up = cross_axis.cross(&self.forward_axis);
-
-        // Second rotation to rotate the up axis to face directly up
-        let actual_up = rotation1 * Vector3::new(0.0, 1.0, 0.0);
-        let rotation2 = UnitQuaternion::rotation_between(&actual_up, &true_up).unwrap();
-
-        // Third rotation to roll the tag
-        let rotation3 = UnitQuaternion::from_scaled_axis(self.forward_axis.normalize() * self.roll);
-
-        rotation3 * rotation2 * rotation1
-    }
-}
-
 /// An observation of the global orientation and position
 /// of the camera that observed an apriltag.
 #[derive(Clone)]
