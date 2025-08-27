fb87ff6b038898dbb031ddb150d1f5f41f35e029
Matthew Ashton Knochel <matthewashtonknochel@gmail.com>
2025-08-27 00:26:04 -0600

    fix occupancy

--- misc/thalassic/src/pcl2occupancy.rs
+++ misc/thalassic/src/pcl2occupancy.rs
@@ -5,15 +5,14 @@ build_shader!(
     const HEIGHTMAP_WIDTH: NonZeroU32 = {{heightmap_width}};
     const CELL_COUNT: NonZeroU32 = {{cell_count}};
     const CELL_SIZE: f32 = {{cell_size}};
-    const MAX_WEIGHT_DISTANCE: f32 = {{max_weight_distance}};
-    const MIN_WEIGHT_DISTANCE: f32 = {{min_weight_distance}};
 
     #[buffer] var<storage, read_write> obstacle_map: array<atomic<u32>, CELL_COUNT>;
-    #[buffer] var<storage, read_write> is_known: array<atomic<u32>, CELL_COUNT>;
+    // maps to already processed cells, this flag is set in the final shader
     #[buffer] var<storage, read_write> is_known: array<atomic<u32>, CELL_COUNT>;
     #[buffer] var<storage, read_write> points: array<vec4f>;
     #[buffer] var<uniform> image_dimensions: vec2u;
     #[buffer] var<uniform> camera_transform: mat4x4f;
+
 
     @compute
     @workgroup_size(8, 8, 1)
@@ -23,38 +22,32 @@ build_shader!(
         if (global_invocation_id.x >= image_dimensions.x || global_invocation_id.y >= image_dimensions.y) {
             return;
         }
+
         let index = global_invocation_id.x + global_invocation_id.y * image_dimensions.x;
         let point = points[index];
+
         if (point.w == 0.0) {
             return;
         }
 
-        let camera_inverse = transpose(mat3x3f(
-            camera_transform[0].xyz,
-            camera_transform[1].xyz,
-            camera_transform[2].xyz
-        ));
-        let camera_translation = camera_transform[3].xyz;
-        let camera_space_point = camera_inverse * (point.xyz - camera_translation);
-
-        let distance_from_camera = abs(camera_space_point.z);
-
-        let weight = exp(-distance_from_camera / MAX_WEIGHT_DISTANCE);
-
         points[index] = vec4f(point.x, point.y, 0.0, point.w);
         let flattened_point = points[index];
+
         if (flattened_point.x < 0.0 || flattened_point.y < 0.0) {
             return;
         }
+
         let x_index = u32(flattened_point.x / CELL_SIZE);
         let y_index = u32(flattened_point.y / CELL_SIZE);
+
         if (x_index >= HEIGHTMAP_WIDTH || y_index >= CELL_COUNT / HEIGHTMAP_WIDTH) {
             return;
         }
+
         let cell_index = y_index * HEIGHTMAP_WIDTH + x_index;
 
-        let weighted_contribution = u32(weight * 100.0);
-        atomicAdd(&obstacle_map[cell_index], max(weighted_contribution, 1u));
+
+        atomicAdd(&obstacle_map[cell_index], 1u);
     }
     "#
 );
