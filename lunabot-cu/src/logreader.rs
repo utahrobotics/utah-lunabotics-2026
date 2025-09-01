897a2d33b41db542e5d11f52370f8691939b80e9
Matthew Ashton Knochel <matthewashtonknochel@gmail.com>
2025-08-31 19:02:22 -0600

    fix log

--- lunabot-cu/src/logreader.rs
+++ lunabot-cu/src/logreader.rs
@@ -1,15 +1,29 @@
-#![feature(let_chains, try_blocks, result_flattening)]
-pub mod tasks;
-pub mod rerun_viz;
-pub mod utils;
+#![feature(let_chains, try_blocks, result_flattening, mpmc_channel)]
 pub mod comms;
+mod motors;
+pub mod rerun_viz;
+mod rp2040;
+pub mod simple_monitor;
+pub mod tasks;
+pub mod utils;
 
+use crossbeam_channel::{Receiver, Sender};
 use cu29::prelude::*;
 use cu29_export::run_cli;
-use simple_motion::StaticNode;
+use cu29_helpers::basic_copper_setup;
+use embedded_common::{ActuatorCommand, FromPicoV3};
+use launcher::ProcessCommand;
+use simple_motion::{ChainBuilder, NodeSerde, StaticNode};
+use std::path::{Path, PathBuf};
 use std::sync::OnceLock;
+use std::thread::sleep;
+use std::time::Duration;
 
 pub static ROOT_NODE: OnceLock<StaticNode> = OnceLock::new();
+const PREALLOCATED_STORAGE_SIZE: Option<usize> = Some(1024 * 1024 * 100);
+
+pub static PICO_TX: OnceLock<Sender<ActuatorCommand>> = OnceLock::new();
+pub static PICO_RX: OnceLock<Receiver<FromPicoV3>> = OnceLock::new();
 
 // This will create the CuMsgs that is specific to your copper project.
 // It is used to instruct the log reader how to decode the logs.
