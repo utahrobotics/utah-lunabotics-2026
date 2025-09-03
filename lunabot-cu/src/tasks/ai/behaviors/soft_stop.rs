860ddc342e8172887310a2d7921ee901f097264d
Matthew Ashton Knochel <matthewashtonknochel@gmail.com>
2025-09-03 04:33:47 -0600

    fix status set

--- lunabot-cu/src/tasks/ai/behaviors/soft_stop.rs
+++ lunabot-cu/src/tasks/ai/behaviors/soft_stop.rs
@@ -1,16 +1,17 @@
-use bonsai_bt::Behavior::{self, Action};
+use bonsai_bt::Behavior::{self, Action, Wait};
 use common::Steering;
 use embedded_common::{Actuator, ActuatorCommand};
 
 use crate::tasks::ai::action::LunabotAction;
 
 pub fn soft_stop_behavior() -> bonsai_bt::Behavior<crate::tasks::ai::action::LunabotAction> {
-    Behavior::While(
+    Behavior::WhileAll(
         Box::new(Action(LunabotAction::IsSoftStop)),
         vec![
             Action(LunabotAction::SetSteering(Steering::new(0., 0.0, 0.0))),
             Action(LunabotAction::SetLift(0)),
             Action(LunabotAction::SetBucket(0)),
+            Wait(1.0),
         ],
     )
 }
--- lunabot-cu/src/tasks/ai/lunabot_ai.rs
+++ lunabot-cu/src/tasks/ai/lunabot_ai.rs
@@ -55,97 +55,6 @@ impl CuTask for LunabotAi {
         input: &Self::Input<'i>,
         output: &mut Self::Output<'o>,
     ) -> cu29::CuResult<()> {
-        let e: Event = UpdateArgs {
-            dt: nanos_to_secs(clock.now().as_nanos() - self.last_tick_nanos),
-        }
-        .into();
-        if let Some(Some(from_lunabase)) = input.payload() {
-            self.bt.blackboard_mut().update_with_msg(from_lunabase);
-        }
-
-        self.bt.tick(&e, &mut |args, blackboard| {
-            let mut status = Running;
-            match *args.action {
-                LunabotAction::SetSteering(steering) => {
-                    blackboard.outgoing_steering_msg = Some(steering);
-                    status = Success;
-                }
-                LunabotAction::SetLastSteering => {
-                    if let Some(steering) = blackboard.last_steering {
-                        blackboard.outgoing_steering_msg = Some(steering);
-                        blackboard.last_steering = None;
-                    }
-                }
-                LunabotAction::SetLastBucket => {
-                    if let Some(value) = blackboard.last_bucket {
-                        let commands = actuator_commands_from_i8(value, Actuator::Bucket);
-                        blackboard
-                            .outgoing_actuator_msg_queue
-                            .push_back(commands[0]);
-                        blackboard
-                            .outgoing_actuator_msg_queue
-                            .push_back(commands[1]);
-                    }
-                }
-                LunabotAction::SetLastLift => {
-                    if let Some(value) = blackboard.last_lift {
-                        let commands = actuator_commands_from_i8(value, Actuator::Lift);
-                        blackboard
-                            .outgoing_actuator_msg_queue
-                            .push_back(commands[0]);
-                        blackboard
-                            .outgoing_actuator_msg_queue
-                            .push_back(commands[1]);
-                    }
-                }
-                LunabotAction::SetBucket(value) => {
-                    let [direction, speed] = actuator_commands_from_i8(value, Actuator::Bucket);
-                    blackboard.last_bucket = None;
-                    blackboard.outgoing_actuator_msg_queue.push_back(direction);
-                    blackboard.outgoing_actuator_msg_queue.push_back(speed);
-                }
-                LunabotAction::SetLift(value) => {
-                    let [direction, speed] = actuator_commands_from_i8(value, Actuator::Lift);
-                    blackboard.last_lift = None;
-                    blackboard.outgoing_actuator_msg_queue.push_back(direction);
-                    blackboard.outgoing_actuator_msg_queue.push_back(speed);
-                }
-                LunabotAction::IsSoftStop => match LUNABOT_STAGE.load() {
-                    LunabotStage::SoftStop => status = Running,
-                    _ => status = Success,
-                },
-                LunabotAction::IsAutonomy => match LUNABOT_STAGE.load() {
-                    LunabotStage::Autonomy => status = Running,
-                    _ => status = Success,
-                },
-                LunabotAction::IsManual => match LUNABOT_STAGE.load() {
-                    LunabotStage::Manual => status = Running,
-                    _ => status = Success,
-                },
-                LunabotAction::None => status = Success,
-                LunabotAction::IsObstacleMapReady => {
-                    if blackboard.latest_obstacle_map.is_some() {
-                        status = Success;
-                    } else {
-                        status = Failure;
-                    }
-                }
-                LunabotAction::IsInOccupiedCell => todo!(),
-                LunabotAction::IsInFreeCell => todo!(),
-                LunabotAction::IsInUnknownCell => todo!(),
-                LunabotAction::CalculatePath => todo!(),
-                LunabotAction::FollowPathFor(meters) => {
-                    // this will be a long running task in a different thread
-                    // the task will live in the jobs folder, and be pollable to get the Status
-                    todo!()
-                }
-                LunabotAction::CheckNavigation => todo!(),
-                LunabotAction::GetUnstuck => todo!(),
-            }
-            (status, args.dt)
-        });
-
-        // TODO: make sure backpressure isnt bad here, maybe we should drain the queue before advancing to the next tick of the blackboard?
         let mut payload = (None, None);
         if let Some(actuator_cmd) = self
             .bt
@@ -160,9 +69,106 @@ impl CuTask for LunabotAi {
         }
         if payload.0.is_some() || payload.1.is_some() {
             output.set_payload(payload);
+            return Ok(());
         } else {
             output.clear_payload();
         }
+
+        let e: Event = UpdateArgs {
+            dt: nanos_to_secs(clock.now().as_nanos() - self.last_tick_nanos),
+        }
+        .into();
+        if let Some(Some(from_lunabase)) = input.payload() {
+            self.bt.blackboard_mut().update_with_msg(from_lunabase);
+        }
+
+        self.bt.tick(&e, &mut |args, blackboard| {
+            let status = match *args.action {
+                LunabotAction::SetSteering(steering) => {
+                    blackboard.outgoing_steering_msg = Some(steering);
+                    Success
+                }
+                LunabotAction::SetLastSteering => {
+                    if let Some(steering) = blackboard.last_steering {
+                        blackboard.outgoing_steering_msg = Some(steering);
+                        blackboard.last_steering = None;
+                    }
+                    Success
+                }
+                LunabotAction::SetLastBucket => {
+                    if let Some(value) = blackboard.last_bucket {
+                        let commands = actuator_commands_from_i8(value, Actuator::Bucket);
+                        blackboard
+                            .outgoing_actuator_msg_queue
+                            .push_back(commands[0]);
+                        blackboard
+                            .outgoing_actuator_msg_queue
+                            .push_back(commands[1]);
+                    }
+                    Success
+                }
+                LunabotAction::SetLastLift => {
+                    if let Some(value) = blackboard.last_lift {
+                        let commands = actuator_commands_from_i8(value, Actuator::Lift);
+                        blackboard
+                            .outgoing_actuator_msg_queue
+                            .push_back(commands[0]);
+                        blackboard
+                            .outgoing_actuator_msg_queue
+                            .push_back(commands[1]);
+                    }
+                    Success
+                }
+                LunabotAction::SetBucket(value) => {
+                    println!("setting bucket");
+                    let [direction, speed] = actuator_commands_from_i8(value, Actuator::Bucket);
+                    blackboard.last_bucket = None;
+                    blackboard.outgoing_actuator_msg_queue.push_back(direction);
+                    blackboard.outgoing_actuator_msg_queue.push_back(speed);
+                    Success
+                }
+                LunabotAction::SetLift(value) => {
+                    println!("setting lift");
+                    let [direction, speed] = actuator_commands_from_i8(value, Actuator::Lift);
+                    blackboard.last_lift = None;
+                    blackboard.outgoing_actuator_msg_queue.push_back(direction);
+                    blackboard.outgoing_actuator_msg_queue.push_back(speed);
+                    Success
+                }
+                LunabotAction::IsSoftStop => match LUNABOT_STAGE.load() {
+                    LunabotStage::SoftStop => Running,
+                    _ => Success,
+                },
+                LunabotAction::IsAutonomy => match LUNABOT_STAGE.load() {
+                    LunabotStage::Autonomy => Running,
+                    _ => Success,
+                },
+                LunabotAction::IsManual => match LUNABOT_STAGE.load() {
+                    LunabotStage::Manual => Running,
+                    _ => Success,
+                },
+                LunabotAction::None => Success,
+                LunabotAction::IsObstacleMapReady => {
+                    if blackboard.latest_obstacle_map.is_some() {
+                        Success
+                    } else {
+                        Failure
+                    }
+                }
+                LunabotAction::IsInOccupiedCell => todo!(),
+                LunabotAction::IsInFreeCell => todo!(),
+                LunabotAction::IsInUnknownCell => todo!(),
+                LunabotAction::CalculatePath => todo!(),
+                LunabotAction::FollowPathFor(meters) => {
+                    // this will be a long running task in a different thread
+                    // the task will live in the jobs folder, and be pollable to get the Status
+                    todo!()
+                }
+                LunabotAction::CheckNavigation => todo!(),
+                LunabotAction::GetUnstuck => todo!(),
+            };
+            (status, args.dt)
+        });
         Ok(())
     }
 }
