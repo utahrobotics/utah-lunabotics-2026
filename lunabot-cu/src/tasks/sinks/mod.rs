36d8cdedd8b74863ba4891da343d064f06aaa9d4
Matthew Ashton Knochel <matthewashtonknochel@gmail.com>
2025-09-08 21:37:49 -0600

    remove source and sink asks for old ai

--- lunabot-cu/src/tasks/sinks/mod.rs
+++ lunabot-cu/src/tasks/sinks/mod.rs
@@ -1,11 +1,9 @@
 mod actuator_controller;
-mod ai_sink;
 mod localizer;
 mod motor_controller;
 mod occupancy_grid_logger;
 
 pub use actuator_controller::*;
-pub use ai_sink::*;
 pub use localizer::*;
 pub use motor_controller::*;
 pub use occupancy_grid_logger::*;
--- lunabot-cu/src/tasks/sources/mod.rs
+++ lunabot-cu/src/tasks/sources/mod.rs
@@ -1,15 +1,13 @@
-pub mod ai_source;
 pub mod l2_imu;
 pub mod l2_pointcloud;
 pub mod lunabase;
+pub mod realsense_occupancy_grid;
 pub mod realsense_pointcloud;
 pub mod udev_monitor;
-pub mod realsense_occupancy_grid;
 
-pub use ai_source::*;
 pub use l2_imu::*;
 pub use l2_pointcloud::*;
 pub use lunabase::*;
+pub use realsense_occupancy_grid::*;
 pub use realsense_pointcloud::*;
 pub use udev_monitor::*;
-pub use realsense_occupancy_grid::*;
