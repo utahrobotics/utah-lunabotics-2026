16d77de5bd3dec070b67bbfa1651212a8a6f9cab
Matthew Ashton Knochel <matthewashtonknochel@gmail.com>
2025-10-05 15:28:56 -0600

    re order build.rs

--- lunabot-cu/build.rs
+++ lunabot-cu/build.rs
@@ -1,11 +1,11 @@
 fn main() {
+    println!(
+        "cargo:rustc-env=LOG_INDEX_DIR={}",
+        std::env::var("OUT_DIR").unwrap()
+    );
     if std::env::var("MUJOCO_STATIC_LINK_DIR").is_err() {
         panic!(
             "Please set the MUJOCO_STATIC_LINK_DIR env variable. Instructions for mujoco installation at https://mujoco-rs.readthedocs.io/en/stable/installation.html#static-linking"
         )
     }
-    println!(
-        "cargo:rustc-env=LOG_INDEX_DIR={}",
-        std::env::var("OUT_DIR").unwrap()
-    );
 }
