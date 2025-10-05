9e3abe329e80713551b01506a377c4bc9fa64a6d
Matthew Ashton Knochel <matthewashtonknochel@gmail.com>
2025-10-05 15:28:11 -0600

    add mujoco dependency

--- lunabot-cu/build.rs
+++ lunabot-cu/build.rs
@@ -1,4 +1,9 @@
 fn main() {
+    if std::env::var("MUJOCO_STATIC_LINK_DIR").is_err() {
+        panic!(
+            "Please set the MUJOCO_STATIC_LINK_DIR env variable. Instructions for mujoco installation at https://mujoco-rs.readthedocs.io/en/stable/installation.html#static-linking"
+        )
+    }
     println!(
         "cargo:rustc-env=LOG_INDEX_DIR={}",
         std::env::var("OUT_DIR").unwrap()
