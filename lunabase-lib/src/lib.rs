08768140fe1cc0afdaa31c80b6cb91fa9a7eb3e1
Matthew Ashton Knochel <matthewashtonknochel@gmail.com>
2025-10-21 20:41:07 -0600

    boilerplate for extension

---
+++ lunabase-lib/src/lib.rs
@@ -0,0 +1,19 @@
+use godot::prelude::*;
+
+struct LunabaseExtension;
+
+#[gdextension]
+unsafe impl ExtensionLibrary for LunabaseExtension {}
+
+#[derive(GodotClass)]
+#[class(base=Node)]
+struct LunabaseConnection {
+    base: Base<Node>,
+}
+
+#[godot_api]
+impl INode for LunabaseConnection {
+    fn init(base: Base<Node>) -> Self {
+        LunabaseConnection { base }
+    }
+}
