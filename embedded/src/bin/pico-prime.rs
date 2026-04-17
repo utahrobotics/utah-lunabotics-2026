5cb6e779930d1b00dcc53191a37081b87021815f
Matthew Ashton Knochel <matthewashtonknochel@gmail.com>
2026-04-17 15:46:59 -0600

    minor fixes

--- embedded/src/bin/pico-prime.rs
+++ embedded/src/bin/pico-prime.rs
@@ -1,32 +1,37 @@
 #![no_std]
 #![no_main]
 
-use defmt::*;
+use defmt::{error, info, warn};
 use embassy_executor::Spawner;
 use embassy_rp::{
     bind_interrupts,
     gpio::{Input, Level, Output},
-    peripherals::USB,
+    peripherals::{UART0, USB},
     pwm::{Config, Pwm, SetDutyCycle},
-    time_driver::init,
+    uart::{Async, Config as UartConfig, InterruptHandler as UartInterruptHandler, Uart},
     usb::{Driver, InterruptHandler},
 };
-use embassy_time::{Duration, Timer};
+use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, channel::Channel};
+use embassy_time::{Duration, Timer, with_timeout};
 use embassy_usb::{
     UsbDevice,
     class::cdc_acm::{CdcAcmClass, Receiver, Sender, State},
 };
 use embedded_common::{
-    Actuator, ActuatorCommand, Direction, FromPico, MAX_MESSAGE_SIZE, PicoError,
+    Actuator, ActuatorCommand, Direction, FromPico, MAX_MESSAGE_SIZE, PicoError, SecondaryRequest,
+    SecondaryResponse, SensorReading,
 };
 use static_cell::StaticCell;
 use {defmt_rtt as _, panic_probe as _};
 bind_interrupts!(struct Irqs {
     USBCTRL_IRQ => InterruptHandler<USB>;
+    UART0_IRQ => UartInterruptHandler<UART0>;
 });
 
 static PACKET_SIZE: u16 = 64;
-const POLL_INTERVAL_MS: u64 = 100;
+const POLL_INTERVAL_MS: u64 = 5;
+
+static SENSOR_READINGS: Channel<CriticalSectionRawMutex, SensorReading, 1> = Channel::new();
 
 struct FaultDetector<'a> {
     fault: Input<'a>,
@@ -68,54 +73,50 @@ impl<'a> ActuatorDriver<'a> {
 async fn main(spawner: Spawner) -> ! {
     let p = embassy_rp::init(Default::default());
 
-    let desired_freq_hz = 20_000;
-    let clock_freq_hz = embassy_rp::clocks::clk_sys_freq();
-    let divider = 1u8;
-    let period = (clock_freq_hz / (desired_freq_hz * divider as u32)) as u16 - 1;
-    let mut c = Config::default();
-    c.top = period;
-    c.divider = divider.into();
-
-    // motor_flt pins
+    // motor_flt pins - GPIO14, 15, 16 = physical pin 19, 20, 21
     let driver_fault_detectors: [FaultDetector<'_>; 3] = [
         FaultDetector {
-            fault: Input::new(p.PIN_10, embassy_rp::gpio::Pull::Up),
+            fault: Input::new(p.PIN_14, embassy_rp::gpio::Pull::Up),
             which: Actuator::Lift,
+        },
+        FaultDetector {
+            fault: Input::new(p.PIN_15, embassy_rp::gpio::Pull::Up),
+            which: Actuator::Bucket,
         },
         FaultDetector {
             fault: Input::new(p.PIN_16, embassy_rp::gpio::Pull::Up),
-            which: Actuator::Bucket,
-        },
-        FaultDetector {
-            fault: Input::new(p.PIN_22, embassy_rp::gpio::Pull::Up),
             which: Actuator::Dumper,
         },
+        // PIN_25 is Motor4 placeholder from Vincent's spec, omitted for now
     ];
     let actuators = [
         ActuatorDriver {
-            sleep: Output::new(p.PIN_11, Level::Low),
-            dir: Output::new(p.PIN_7, Level::Low),
-            pwm: Pwm::new_output_a(p.PWM_SLICE4, p.PIN_8, c.clone()),
+            // GPIO8 = physical pin 11, GPIO0 = pin 1
+            sleep: Output::new(p.PIN_8, Level::Low),
+            dir: Output::new(p.PIN_0, Level::Low),
+            pwm: Pwm::new_output_b(p.PWM_SLICE0, p.PIN_1, Config::default()),
             which: Actuator::Lift,
         },
         ActuatorDriver {
-            sleep: Output::new(p.PIN_17, Level::Low),
-            dir: Output::new(p.PIN_13, Level::Low),
-            pwm: Pwm::new_output_a(p.PWM_SLICE7, p.PIN_14, c.clone()),
+            // GPIO 9 and 2 = pin 12 and 4
+            sleep: Output::new(p.PIN_9, Level::Low),
+            dir: Output::new(p.PIN_2, Level::Low),
+            pwm: Pwm::new_output_b(p.PWM_SLICE1, p.PIN_3, Config::default()),
             which: Actuator::Bucket,
         },
         ActuatorDriver {
-            sleep: Output::new(p.PIN_23, Level::Low),
-            dir: Output::new(p.PIN_19, Level::Low),
-            pwm: Pwm::new_output_a(p.PWM_SLICE2, p.PIN_20, c.clone()),
+            // GPIO 10 and 4 = pin 14 and 6
+            sleep: Output::new(p.PIN_10, Level::Low),
+            dir: Output::new(p.PIN_4, Level::Low),
+            pwm: Pwm::new_output_b(p.PWM_SLICE2, p.PIN_5, Config::default()),
             which: Actuator::Dumper,
         },
         // 4th motor driver slot is currently unused
-        // ActuatorDriver {
-        //     sleep: Output::new(p.PIN_29, Level::Low),
-        //     fault: Input::new(p.PIN_28, embassy_rp::gpio::Pull::Up),
-        //     dir: Output::new(p.PIN_25, Level::Low),
-        //     pwm: Pwm::new_output_a(p.PWM_SLICE5, p.PIN_26, Config::default()),
+        // ActuatorDriver { // GPIO 15 and 9
+        //     sleep: Output::new(p.PIN_15, Level::Low),
+        //     fault: Input::new(p.PIN_25, embassy_rp::gpio::Pull::Up),
+        //     dir: Output::new(p.PIN_9, Level::Low),
+        //     pwm: Pwm::new_output_a(p.PWM_SLICE5, p.PIN_7, Config::default()),
         //     which: ...,
         // },
     ];
@@ -158,6 +159,15 @@ async fn main(spawner: Spawner) -> ! {
     info!("about to spawn usb task");
     spawner.spawn(usb_task(usb)).unwrap();
 
+    let uart = {
+        let mut cfg = UartConfig::default();
+        cfg.baudrate = 115200;
+        Uart::new(p.UART0, p.PIN_12, p.PIN_13, Irqs, p.DMA_CH0, p.DMA_CH1, cfg)
+    };
+    static SECONDARY_UART: StaticCell<Uart<'static, embassy_rp::uart::Async>> = StaticCell::new();
+    let uart = SECONDARY_UART.init(uart);
+    spawner.spawn(secondary_poll_loop(uart)).unwrap();
+
     let (class_tx, class_rx) = class.split();
 
     static ACTUATORS: StaticCell<[ActuatorDriver<'static>; 3]> = StaticCell::new();
@@ -198,7 +208,17 @@ async fn usb_tx_loop(
                 }
             }
 
-            // TODO: once secondary pico is implemented, read any sensor stuff off of it and send it over to the host here
+            if let Ok(_sensors) = SENSOR_READINGS.try_receive() {
+                // TODO: send sensor readings to host
+                // let serialized = FromPico::Reading([FromIMU; 4], sensors).serialize();
+                // let len = cobs::encode(&serialized, &mut stuffed);
+                // for chunk in stuffed[..len + 1].chunks(64) {
+                //     if let Err(e) = writer.write_packet(chunk).await {
+                //         error!("[COBS ERROR] {:?}", e);
+                //         break 'writer;
+                //     }
+                // }
+            }
 
             Timer::after(Duration::from_millis(POLL_INTERVAL_MS)).await;
         }
@@ -273,6 +293,61 @@ async fn usb_rx_loop(
                 }
             }
         }
+    }
+}
+
+/// Constantly polls all 11 active MUX channels and publishes reading to SENSOR_READINGS
+#[embassy_executor::task]
+async fn secondary_poll_loop(uart: &'static mut Uart<'static, embassy_rp::uart::Async>) {
+    const RESPONSE_TIMEOUT_MS: u64 = 50;
+    let mut readings = [0u16; SensorReading::CHANNEL_COUNT];
+    loop {
+        for ch in 0u8..SensorReading::CHANNEL_COUNT as u8 {
+            let req = SecondaryRequest { mux_address: ch }.serialize();
+            if uart.write(&req).await.is_err() {
+                error!("failed to write to secondary pico");
+                readings[ch as usize] = 0; // output 0 to avoid sending a completely empty reading
+                continue;
+            }
+            let mut response_buf = [0u8; SecondaryResponse::SIZE];
+            match with_timeout(
+                Duration::from_millis(RESPONSE_TIMEOUT_MS),
+                uart.read(&mut response_buf),
+            )
+            .await
+            {
+                Ok(Ok(_)) => {
+                    readings[ch as usize] = SecondaryResponse::deserialize(response_buf).adc_value
+                }
+                Ok(Err(e)) => {
+                    error!("secondary UART read error ch {}: {:?}", ch, e);
+                    readings[ch as usize] = 0;
+                }
+                Err(_) => {
+                    error!("secondary response timeout ch {}", ch);
+                    readings[ch as usize] = 0;
+                }
+            }
+        }
+
+        let sensor = SensorReading {
+            m1_cs: readings[0],
+            m2_cs: readings[1],
+            m3_cs: readings[2],
+            m4_cs: readings[3],
+            m1_therm: readings[4],
+            m2_therm: readings[5],
+            m3_therm: readings[6],
+            m4_therm: readings[7],
+            drive1_he: readings[8],
+            drive2_he: readings[9],
+            amb_therm: readings[10],
+        };
+
+        info!("sensor readings: {:?}", sensor);
+
+        SENSOR_READINGS.try_send(sensor).ok();
+        Timer::after(Duration::from_millis(POLL_INTERVAL_MS)).await;
     }
 }
 
--- embedded/src/bin/pico-secondary.rs
+++ embedded/src/bin/pico-secondary.rs
@@ -4,12 +4,13 @@
 use defmt::*;
 use embassy_executor::Spawner;
 use embassy_rp::{
-    adc::{Adc, Config as AdcConfig, InterruptHandler as AdcInterruptHandler, Channel},
+    adc::{Adc, Channel, Config as AdcConfig, InterruptHandler as AdcInterruptHandler},
     bind_interrupts,
     gpio::{Level, Output},
-    peripherals::{UART1, UART0},
+    peripherals::UART0,
     uart::{Config as UartConfig, InterruptHandler as UartInterruptHandler, Uart},
 };
+use embassy_time::{Duration, Timer, with_timeout};
 use embedded_common::{SecondaryRequest, SecondaryResponse};
 use {defmt_rtt as _, panic_probe as _};
 
@@ -21,9 +22,8 @@ bind_interrupts!(struct Irqs {
 const UART_IDLE_TIMEOUT_MS: u64 = 1000;
 const MUX_SETTLE_US: u64 = 10;
 
-
 #[embassy_executor::main]
-async fn main(spawner: Spawner) {
+async fn main(_spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
 
     info!("Starting Secondary Pico");
@@ -44,11 +44,10 @@ async fn main(spawner: Spawner) {
     );
 
     // Initialize ADC
-    let mut adc_config = AdcConfig::default();
+    let adc_config = AdcConfig::default();
     let mut adc = Adc::new(p.ADC, Irqs, adc_config);
-    let mut adc_pin = Channel::new_pin(p.PIN_26, embassy_rp::gpio::Pull::None); // ADC0 is pin 26 
-    
-    
+    let mut adc_pin = Channel::new_pin(p.PIN_27, embassy_rp::gpio::Pull::None); // ADC1 is pin 27
+
     // Initialize MUX  pins
     let mut mux_s0 = Output::new(p.PIN_19, Level::Low); // physical pin 25= GPIO 19
     let mut mux_s1 = Output::new(p.PIN_20, Level::Low); // physical pin 26= GPIO 20
@@ -56,43 +55,88 @@ async fn main(spawner: Spawner) {
     let mut mux_s3 = Output::new(p.PIN_22, Level::Low); // physical pin 29 = GPIO22
 
     let mut rx_buf = [0u8; SecondaryRequest::SIZE];
-    
-
 
     loop {
-        // Wait for a request representing the MUX address
-        match uart.read(&mut rx_buf).await {
-            Ok(_) => {
-                // Set MUX  pins
-                mux = rx_buf[0];
-                // Read ADC
-                    match adc.read(&mut adc_pin).await {
-                        Ok(value) => {
-                            info!("ADC value: {}", value);
-                            let resp = SecondaryResponse { adc_value: value };
-                            let tx_buf = resp.serialize();
-                            if let Err(e) = uart.write(&tx_buf).await {
-                                error!("Failed to write UART response: {:?}", e);
-                            }
-                        }
-                        Err(_e) => {
-                            error!("ADC read failed");
-                        }
-                    }
-                } else {
-                    error!("Failed to deserialize request packet");
+        match with_timeout(
+            Duration::from_millis(UART_IDLE_TIMEOUT_MS),
+            uart.read(&mut rx_buf),
+        )
+        .await
+        {
+            Err(_timeout) => {
+                // Primary is idle or disconnected
+                continue;
+            }
+            Ok(Err(e)) => {
+                // UART error, next byte will correct
+                error!("UART read error: {:?}", e);
+                continue;
+            }
+            Ok(Ok(_)) => {}
+        }
+
+        let req = match SecondaryRequest::deserialize(rx_buf) {
+            Ok(r) => r,
+            Err(e) => {
+                error!("Invalid request byte 0x{:02X}: {}", rx_buf[0], e);
+                continue;
+            }
+        };
+
+        info!("request mux channel {}", req.mux_address);
+
+        set_mux(
+            req.mux_address,
+            &mut mux_s0,
+            &mut mux_s1,
+            &mut mux_s2,
+            &mut mux_s3,
+        );
+        Timer::after(Duration::from_micros(MUX_SETTLE_US)).await;
+
+        match adc.read(&mut adc_pin).await {
+            Ok(value) => {
+                info!("ADC ch{} = {}", req.mux_address, value);
+                let tx_buf = SecondaryResponse { adc_value: value }.serialize();
+
+                if let Err(e) = uart.write(&tx_buf).await {
+                    error!("UART write error: {:?}", e);
                 }
             }
-            Err(e) => {
-                error!("UART read error: {:?}", e);
+            Err(_) => {
+                error!("adc read failed for channel {}", req.mux_address);
             }
         }
     }
+}
 
-
-fn set_mux(addr: u8, s0: &mut Output, s1: &mut Output, s2: &mut Output, s3: &mut Output) {
-    s0.set_level(if addr & 0b0001 != 0 { Level::High } else { Level::Low });
-    s1.set_level(if addr & 0b0010 != 0 { Level::High } else { Level::Low });
-    s2.set_level(if addr & 0b0100 != 0 { Level::High } else { Level::Low });
-    s3.set_level(if addr & 0b1000 != 0 { Level::High } else { Level::Low });
+/// Bit 0 -> S0, bit 1 -> S1, bit 2 -> S2, bit 3 -> S3.
+#[inline]
+fn set_mux(
+    addr: u8,
+    s0: &mut Output<'_>,
+    s1: &mut Output<'_>,
+    s2: &mut Output<'_>,
+    s3: &mut Output<'_>,
+) {
+    s0.set_level(if addr & 0b0001 != 0 {
+        Level::High
+    } else {
+        Level::Low
+    });
+    s1.set_level(if addr & 0b0010 != 0 {
+        Level::High
+    } else {
+        Level::Low
+    });
+    s2.set_level(if addr & 0b0100 != 0 {
+        Level::High
+    } else {
+        Level::Low
+    });
+    s3.set_level(if addr & 0b1000 != 0 {
+        Level::High
+    } else {
+        Level::Low
+    });
 }
