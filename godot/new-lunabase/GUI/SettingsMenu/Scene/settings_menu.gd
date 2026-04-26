0566ea23ff639c0e68963c9e45c5abe45eed4bdb
Karl-Jansky <alatorre.carloseduardo@gmail.com>
2026-04-26 16:15:44 -0600

    ignore nav actions

--- godot/new-lunabase/GUI/SettingsMenu/Scene/settings_menu.gd
+++ godot/new-lunabase/GUI/SettingsMenu/Scene/settings_menu.gd
@@ -8,6 +8,17 @@ signal updated_control_scheme
 signal touch_screen_controls_changed(enabled: bool)
 
 var touch_screen_controls_enabled: bool = false
+var disable_ui_inputs := true
+const UI_NAV_ACTIONS: PackedStringArray = [
+	"ui_up",
+	"ui_down",
+	"ui_left",
+	"ui_right",
+	"ui_accept",
+	"ui_cancel",
+	"ui_focus_next",
+	"ui_focus_prev",
+]
 
 func set_touch_screen_controls(enabled: bool) -> void:
 	if touch_screen_controls_enabled == enabled:
@@ -16,10 +27,24 @@ func set_touch_screen_controls(enabled: bool) -> void:
 	touch_screen_controls_changed.emit(enabled)
 
 func _ready() -> void:
+	if disable_ui_inputs:
+		_disable_non_pointer_ui_navigation()
 	control_scheme_switcher.updated_control_scheme.connect(_on_controls_updated)
 	control_scheme_switcher.close_settings_menu.connect(_on_settings_button_toggled)
 
+
+func _disable_non_pointer_ui_navigation() -> void:
+	for action_name in UI_NAV_ACTIONS:
+		if not InputMap.has_action(action_name):
+			continue
+		var action_events := InputMap.action_get_events(action_name)
+		for event in action_events:
+			if event is InputEventKey or event is InputEventJoypadButton or event is InputEventJoypadMotion:
+				InputMap.action_erase_event(action_name, event)
+
 func _on_controls_updated():
+	if disable_ui_inputs:
+			_disable_non_pointer_ui_navigation()
 	updated_control_scheme.emit()
 
 func toggle_menu_visibility(menu : Control, make_visible : bool):
