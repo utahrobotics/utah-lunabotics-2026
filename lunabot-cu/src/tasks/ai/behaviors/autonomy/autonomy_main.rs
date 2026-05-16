83763926432288c350d2d3894624affd5758a0e2
Matthew Ashton Knochel <matthewashtonknochel@gmail.com>
2026-05-16 11:18:57 -0400

    add forward steering before dig

--- lunabot-cu/src/tasks/ai/behaviors/autonomy/autonomy_main.rs
+++ lunabot-cu/src/tasks/ai/behaviors/autonomy/autonomy_main.rs
@@ -1,20 +1,24 @@
 use bonsai_bt::Behavior::{self, Action, Sequence, Wait, While};
 use common::Steering;
 
-use crate::tasks::ai::{action::LunabotAction, behaviors::autonomy::navigate::{Arena, NavigationGoal, back_up_for, hardcoded_navigation, navigate_behavior}};
-
+use crate::tasks::ai::{action::LunabotAction, behaviors::{autonomy::navigate::{Arena, NavigationGoal, back_up_for, navigate_behavior}, steer_for_n_seconds}};
+const DIG_TRAVERSAL_TIME: f64 = 4.0;
 pub fn autonomy_main(arena: Arena) -> Behavior<LunabotAction> {
     While(
         Box::new(Action(LunabotAction::IsAutonomy)),
         vec![
             // 1. go to dig site
-            navigate_behavior(NavigationGoal::DigSite(arena)),
+            Action(LunabotAction::SetBTStatusMsg(format!("Moving to dig site for {:?}", arena))),
+            steer_for_n_seconds(Steering::new(1.0, 1.0, 1000.0), DIG_TRAVERSAL_TIME),
+            // navigate_behavior(NavigationGoal::DigSite(arena)),
+            Action(LunabotAction::SetBTStatusMsg(format!("DIGGING"))),
             open_loop_dig_from_starting(),
             // 2. dig
             // Action(LunabotAction::Dig),
 
             // 3. Navigate to dump site
             navigate_behavior(NavigationGoal::DumpSite(arena)),
+            Action(LunabotAction::SetBTStatusMsg(format!("DUMPING"))),
             open_loop_dump_from_zero(),
             back_up_for(0.5),
             Wait(5.5),
@@ -22,102 +26,6 @@ pub fn autonomy_main(arena: Arena) -> Behavior<LunabotAction> {
             // Action(LunabotAction::Dump),
         ],
     )
-    // TEST DIG AND DUMP
-    // While(
-    //     Box::new(Action(LunabotAction::IsAutonomy)),
-    //     vec![
-    //         Action(LunabotAction::Dig),
-    //         Wait(5.0),
-    //         Action(LunabotAction::Dump),
-    //         Wait(5.0),
-    //     ]
-    // )
-    // TEST PID DUMP
-    // While(
-    //     Box::new(Action(LunabotAction::IsAutonomy)),
-    //     vec![
-    //         Action(LunabotAction::SetDumperAngle(1.0)),
-    //         Action(LunabotAction::Yield),
-    //         Wait(1.0)
-    //     ]
-    // )
-    // OPEN LOOP DIG-DUMP
-    // While(
-    //     Box::new(Action(LunabotAction::IsAutonomy)),
-    //     vec![
-    //         Wait(0.25), // Safety wait
-    //         Action(LunabotAction::SetBucket((0.9 * i8::MAX as f64) as i8)),
-    //         Wait(2.5),
-    //         Action(LunabotAction::SetBucket(0)),
-    //         Action(LunabotAction::SetLift((-0.75 * i8::MAX as f64) as i8)),
-    //         Wait(0.8),
-    //         Action(LunabotAction::SetLift(0)),
-    //         Action(LunabotAction::SetLift((-0.1 * i8::MAX as f64) as i8)),
-    //         Wait(3.0),
-    //         Action(LunabotAction::SetLift(0)),
-    //         Action(LunabotAction::SetLift((-0.2 * i8::MAX as f64) as i8)),
-    //         Action(LunabotAction::SetBucket((0.2 * i8::MAX as f64) as i8)),
-    //         While(
-    //             Box::new(Wait(3.0)),
-    //             vec![
-    //                 Action(LunabotAction::SetSteering(Steering::new_ik(-0.25, 0.0, 8000.0))),
-    //                 Wait(0.01)
-    //             ]
-    //         ),
-    //         Action(LunabotAction::SetLift(0)),
-    //         Action(LunabotAction::SetBucket(0)),
-    //         Action(LunabotAction::SetSteering(Steering::new_ik(0.0, 0.0, 8000.0))),
-    //         Action(LunabotAction::SetBucket((-0.1 * i8::MAX as f64) as i8)),
-    //         While(
-    //             Box::new(Wait(0.75)),
-    //             vec![
-    //                 Action(LunabotAction::SetSteering(Steering::new_ik(-0.25, 0.0, 8000.0))),
-    //                 Wait(0.01)
-    //             ]
-    //         ),
-    //         Action(LunabotAction::SetSteering(Steering::new_ik(0.0, 0.0, 8000.0))),
-    //         Wait(0.75),
-    //         Action(LunabotAction::SetBucket((-0.75 * i8::MAX as f64) as i8)),
-    //         Wait(2.5),
-    //         Action(LunabotAction::SetBucket(0)),
-    //         Action(LunabotAction::SetLift((0.9 * i8::MAX as f64) as i8)),
-    //         Action(LunabotAction::SetBucket((0.5 * i8::MAX as f64) as i8)),
-    //         Wait(6.0),
-    //         Action(LunabotAction::SetLift(0)),
-    //         Action(LunabotAction::SetBucket(0)),
-    //         Action(LunabotAction::SetLift((0.1 * i8::MAX as f64) as i8)),
-    //         Action(LunabotAction::SetBucket((-0.8 * i8::MAX as f64) as i8)),
-    //         Wait(3.0),
-    //         Action(LunabotAction::SetLift(0)),
-    //         Action(LunabotAction::SetBucket(0)),
-    //         Wait(4.0), // wait for regolith to pour
-    //         Action(LunabotAction::SetBucket((0.9 * i8::MAX as f64) as i8)),
-    //         Wait(2.0),
-    //         Action(LunabotAction::SetBucket(0)),
-    //         Action(LunabotAction::SetLift((-0.35 * i8::MAX as f64) as i8)),
-    //         Action(LunabotAction::SetBucket((0.9 * i8::MAX as f64) as i8)),
-    //         Wait(2.0),
-    //         Action(LunabotAction::SetLift(0)),
-    //         Action(LunabotAction::SetBucket(0)),
-
-    //         // Traverse and dump
-
-    //         Wait(1.5),
-    //         While(
-    //             Box::new(Wait(1.0)),
-    //             vec![
-    //                 Action(LunabotAction::SetSteering(Steering::new_ik(1.0, 0.0, 8000.0))),
-    //                 Wait(0.01)
-    //             ]
-    //         ),
-    //         Action(LunabotAction::SetSteering(Steering::new_ik(0.0, 0.0, 8000.0))),
-    //         Wait(1.5),
-    //         Action(LunabotAction::SetDumper((0.75 * i8::MAX as f64) as i8)),
-    //         Wait(5.0),
-    //         Action(LunabotAction::SetDumper(0)),
-    //         Wait(10.0),
-    //     ]
-    // )
 }
 
 
--- lunabot-cu/src/tasks/ai/behaviors/helper_nodes.rs
+++ lunabot-cu/src/tasks/ai/behaviors/helper_nodes.rs
@@ -1,21 +1,28 @@
 #![allow(unused, dead_code)]
 /// these are modeled after some of nav2's nodes.
-
-use bonsai_bt::Behavior::{self, Invert, Race, Select, Wait};
+use bonsai_bt::Behavior::{self, Action, Invert, Race, Select, Sequence, Wait, While};
+use common::Steering;
 
 use crate::tasks::ai::action::LunabotAction;
 
 /// tries to run the retry_me behavior retries times, and stops on the first success
 pub fn recovery_node(retries: usize, retry_me: Behavior<LunabotAction>) -> Behavior<LunabotAction> {
-    let retry_sequence = (0..retries).map(|_| {
-        retry_me.clone()
-    }).collect::<Vec<Behavior<LunabotAction>>>();
+    let retry_sequence: Vec<Behavior<LunabotAction>> = (0..retries)
+        .map(|_| retry_me.clone())
+        .collect::<Vec<Behavior<LunabotAction>>>();
     Select(retry_sequence)
 }
 
 pub fn with_timeout<A: Clone>(behavior: Behavior<A>, timeout: f64) -> Behavior<A> {
-    Race(vec![
-        behavior,
-        Invert(Box::new(Wait(timeout))),
+    Race(vec![behavior, Invert(Box::new(Wait(timeout)))])
+}
+
+pub fn steer_for_n_seconds(steering: Steering, n: f64) -> Behavior<LunabotAction> {
+    Sequence(vec![
+        While(
+            Box::new(Wait(n)),
+            vec![Action(LunabotAction::SetSteering(steering)), Wait(0.05)],
+        ),
+        Action(LunabotAction::SetSteering(Steering::new(0.0, 0.0, 1000.0))),
     ])
 }
